130f19135b21dc605b794d024b6c9c9672e81dd9
ajay.ashubaba <ajay.ashubaba@gmail.com>
2018-06-24 05:50:35 -0400

    k

--- src/services/ok/ok.service.js
+++ src/services/ok/ok.service.js
@@ -1,11 +1,11 @@
 const myService = {
+
   find(params) {
-setTimeout(()=>{console.log('emitted');this.emit('status',{kabua:'abcd'})},2000)
+this.events=['status'];setTimeout(()=>{console.log('emitted');this.emit('status',{kabua:'abcd'})},2000)
     return Promise.resolve(344);
   }}
       
   module.exports=function(app){
-        this.events = ['status']
     
 app.use('/ok', myService);
 
